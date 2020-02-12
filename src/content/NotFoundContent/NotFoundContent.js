3a57e86462412a2d55cb1db7d89b317ad3d5b673
Pratik KC <pratikkc@pratiks-air.lan>
2020-02-11 19:58:48 -0500

    added cards

--- src/content/NotFoundContent/NotFoundContent.js
+++ src/content/NotFoundContent/NotFoundContent.js
@@ -1,39 +1,63 @@
 import React, { Component } from 'react';
-
 import PropTypes from 'prop-types';
-
 import { Link } from 'react-router-dom';
-
 import { withStyles } from '@material-ui/core/styles';
-
 import Fab from '@material-ui/core/Fab';
-
 import FindIcon from '@material-ui/icons/FindInPage';
 import HomeIcon from '@material-ui/icons/Home';
-
 import EmptyState from '../../layout/EmptyState/EmptyState';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-const styles = (theme) => ({
-  emptyStateIcon: {
-    fontSize: theme.spacing(12)
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
   },
-
-  button: {
-    marginTop: theme.spacing(1)
+  media: {
+    height: 140,
   },
-
-  buttonIcon: {
-    marginRight: theme.spacing(1)
-  }
 });
 
-class NotFoundContent extends Component {
-  render() {
-    // Styling
-    const { classes } = this.props;
+export default function MediaCard() {
+  const classes = useStyles();
 
-    return (
-      <EmptyState
+  return (
+
+    <div> 
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+
+    <EmptyState
         icon={<FindIcon className={classes.emptyStateIcon} color="action" />}
         title="Content Not Found"
         description="The requested URL was not found on this server"
@@ -43,12 +67,34 @@ class NotFoundContent extends Component {
           </Fab>
         }
       />
-    );
-  }
+
+<Card className={classes.root}>
+<CardActionArea>
+  <CardMedia
+    className={classes.media}
+    image="/static/images/cards/contemplative-reptile.jpg"
+    title="Contemplative Reptile"
+  />
+  <CardContent>
+    <Typography gutterBottom variant="h5" component="h2">
+      Lizard
+    </Typography>
+    <Typography variant="body2" color="textSecondary" component="p">
+      Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+      across all continents except Antarctica
+    </Typography>
+  </CardContent>
+</CardActionArea>
+<CardActions>
+  <Button size="small" color="primary">
+    Share
+  </Button>
+  <Button size="small" color="primary">
+    Learn More
+  </Button>
+</CardActions>
+</Card>
+
+</div>
+  );
 }
-
-NotFoundContent.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(styles)(NotFoundContent);
